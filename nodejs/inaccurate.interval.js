64c23a226a7afe6b44ab6f84d04b6e87bdc9d56e
jakemadness <j4kem4dness@live.com>
2019-02-13 17:19:00 +0000

    Create inaccurate.interval.js

---
+++ nodejs/inaccurate.interval.js
@@ -0,0 +1,29 @@
+/*
+   @https://github.com/jakemadness/inaccurate-interval/tree/master/nodejs
+*/
+module.exports = function(callback, min, max) {
+   var self        = this;
+   self.callback   = callback;
+   self.timeout    = false;
+   self.running    = false;
+   self.randomTime = function() {
+      return Math.random() * (max - min) + min;
+   };
+   self.next = function() {
+      if ( self.running ) {
+         self.timeout = setTimeout(function() {
+            self.callback();
+            self.next();
+         }, self.randomTime());
+      }
+   };
+   self.start = function() {
+      self.running = true;
+      self.next();
+   };
+   self.stop = function() {
+      clearTimeout(self.timeout);
+      self.running = false;
+   };
+   self.start();
+};
