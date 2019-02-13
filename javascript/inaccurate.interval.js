76497aa41f297e67fe6d3aa90db68def0a2cb315
jakemadness <j4kem4dness@live.com>
2019-02-13 17:15:00 +0000

    Update inaccurate.interval.js

--- javascript/inaccurate.interval.js
+++ javascript/inaccurate.interval.js
@@ -1,7 +1,7 @@
 /*
    @https://github.com/jakemadness/inaccurate-interval/tree/master/javascript
 */
-var inaccurateInterval = function(callback, min, max) {
+window.inaccurateInterval = function(callback, min, max) {
    var self        = this;
    self.callback   = callback;
    self.timeout    = false;
