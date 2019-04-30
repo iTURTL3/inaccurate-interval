d8e751ec35210f48e6446e7d3434fbdfe06589f5
jakemadness <j4kem4dness@live.com>
2019-04-30 16:20:46 +0100

    Update inaccurate.interval.js

--- inaccurate.interval.js
+++ inaccurate.interval.js
@@ -4,28 +4,28 @@
 window.inaccurateInterval = function(callback, min, max) {
    var self        = this;
    self.callback   = callback;
+   self.min        = min;
+   self.max        = max;
    self.timeout    = false;
    self.running    = false;
    self.randomTime = function() {
-      return Math.random() * (max - min) + min;
+      return Math.random() * (self.max - self.min) + self.min;
    };
    self.next = function() {
-      self.timeout = setTimeout(function() {
-         self.callback();
-         self.next();
-      }, self.randomTime());
+      if ( self.running ) {
+         self.timeout = setTimeout(function() {
+            self.callback();
+            self.next();
+         }, self.randomTime());
+      }
    };
    self.start = function() {
-      if ( !self.running ) {
-         self.running = true;
-         self.next();
-      }
+      self.running = true;
+      self.next();
    };
    self.stop = function() {
-      if ( self.running ) {
-         clearTimeout(self.timeout);
-         self.running = false;
-      }
+      clearTimeout(self.timeout);
+      self.running = false;
    };
    self.start();
 };
